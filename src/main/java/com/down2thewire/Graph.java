f83a367a8dd456c8df839b8ac469430664bded8a
mehoney11 <friends2honey@gmail.com>
2023-08-23 22:15:22 -0400

    Created Abstract Class for geomodel & Route Class

---
+++ src/main/java/com/down2thewire/Graph.java
@@ -0,0 +1,33 @@
+package com.down2thewire;
+
+
+
+
+import java.util.List;
+
+abstract class Graph {
+
+    /*
+    abstract void addNode();
+    abstract void removeNode(Node);
+
+    abstract void addGraph(Graph);
+    abstract Boolean isUnique(Node);
+    //in the addGraph - adding two graphs so that there are no duplicate nodes
+    abstract Graph cloneGraph(Graph);
+    //
+    abstract void printGraph(Graph);
+    abstract List<Node> find(String s);
+    //effectively same as getNode in vertex;
+    //if it doesn't find match returns list of nodes ;string[nodes]
+    abstract List<Node> find(Location l);
+
+    abstract Node findNext(String s);
+
+    //overloaded with location
+    abstract Node findNext(Location l);
+
+    abstract Node getNextNode(Location l);
+*/
+
+}
