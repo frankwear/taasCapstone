1b965c4241266f90a01871ddbb560c0822fe7046
mehoney11 <friends2honey@gmail.com>
2023-11-15 19:42:49 -0500

    JSON Data Sent to FE

--- webapp/src/main/webapp/index.jsp
+++ webapp/src/main/webapp/index.jsp
@@ -1,4 +1,5 @@
 <%@ page contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" %>
+<%@ page import="java.util.Random" %>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,68 +7,84 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Transportation Selection Form</title>
     <link rel="stylesheet" href="styles.css">
-
 </head>
 <body>
-    <h1>Transportation Selection Form</h1>
-    <p>Please fill out the form below:</p>
-    <div class="container">
-        <form action="CaptureDataServlet" method="post" enctype="multipart/form-data">
-            <div class="form-row-horizontal">
-                <div class="form-row">
-                    <label for="origin">Origin:</label>
-                    <input type="text" id="origin" name="origin" placeholder="Enter Origin" required>
-                </div>
-                <div class="form-row">
-                    <label for="addStop">Add Stop:</label>
-                    <input type="text" id="addStop" name="addStop" placeholder="Enter stop">
-                </div>
-                <div class="form-row">
-                    <label for="destination">Destination:</label>
-                    <input type="text" id="destination" name="destination" placeholder="Enter destination" required>
-                </div>
-                <div class="form-row">
-                    <label for="walking">Walking (1 - Must Use):</label>
-                    <input type="number" id="walking" name="walking" min="1" max="4" value="1" required>
-                </div>
-                <div class="form-row">
-                    <label for="bicycling">Bicycling (1 - Must Use):</label>
-                    <input type="number" id="bicycling" name="bicycling" min="1" max="4" value="3" required>
-                </div>
-                <div class="form-row">
-                    <label for="drive">Drive (1 - Must Use):</label>
-                    <input type="number" id="drive" name="drive" min="1" max="4" value="1" required>
-                </div>
-                <div class="form-row">
-                    <label for="transit">Transit (1 - Must Use):</label>
-                    <input type="number" id="transit" name="transit" min="1" max="4" value="2" required>
-                </div>
+<div class="navbar">
+    <ul>
+        <li><a href="#">User Profile</a></li>
+        <li><a href="#">Saved Locations</a></li>
+        <li><a href="#">Help</a></li>
+        <li class="logout"><a href="#">Logout</a></li>
+    </ul>
+</div>
+<h1 align="center">Transportation Selection Form</h1>
+<div class="container">
+    <form action="CaptureDataServlet" method="post" enctype="multipart/form-data">
+        <div class="form-row-horizontal">
+            <div class="form-row">
+                <label for="origin">Origin:</label>
+                <input type="text" id="origin" name="origin" placeholder="Enter Origin" required>
             </div>
-
-            <div class="form">
-                <label>Select an option:</label>
-                <div class="radio-options-horizontal" >
-                    <label><input type="radio" name="option" value="cost" onclick="setSelectedOption(this)">Cost</label>
-                    <label><input type="radio" name="option" value="speed" onclick="setSelectedOption(this)" checked>Speed </label>
-                    <label><input type="radio" name="option" value="best-route" onclick="setSelectedOption(this)">Best route</label>
-                    <label><input type="radio" name="option" value="low-carbon" onclick="setSelectedOption(this)">Low Carbon</label>
-                    <label><input type="radio" name="option" value="fewer-transfers" onclick="setSelectedOption(this)">Fewer transfers</label>
-                </div>
+            <div class="form-row">
+                <label for="addStop">Add Stop:</label>
+                <input type="text" id="addStop" name="addStop" placeholder="Enter stop">
             </div>
-            <button type="button" id="submit" onclick="submitForm(), populateDropdown()">Go</button>
-        </form>
-<h1>Select a Route from below options </h1>
-        <button id="loadRoutesButton">Load Routes</button>
-        <select id="routesDropdown"></select>
-        <div class="map-placeholder">
-            <img src="map_placeholder_image.jpeg" alt="Map placeholder" width="100%" height="100%">
+            <div class="form-row">
+                <label for="destination">Destination:</label>
+                <input type="text" id="destination" name="destination" placeholder="Enter destination" required>
+            </div>
+            <div class="form-row">
+                <label for="walking">Walking (1 - Must Use):</label>
+                <input type="number" id="walking" name="walking" min="1" max="4" value="1" required>
+            </div>
+            <div class="form-row">
+                <label for="bicycling">Bicycling (1 - Must Use):</label>
+                <input type="number" id="bicycling" name="bicycling" min="1" max="4" value="3" required>
+            </div>
+            <div class="form-row">
+                <label for="drive">Drive (1 - Must Use):</label>
+                <input type="number" id="drive" name="drive" min="1" max="4" value="1" required>
+            </div>
+            <div class="form-row">
+                <label for="transit">Transit (1 - Must Use):</label>
+                <input type="number" id="transit" name="transit" min="1" max="4" value="2" required>
+            </div>
         </div>
+
+        <div class="form">
+            <label>Select an option:</label>
+            <div class="radio-options-horizontal">
+                <label><input type="radio" name="option" value="cost" onclick="setSelectedOption(this)">Cost</label>
+                <label><input type="radio" name="option" value="speed" onclick="setSelectedOption(this)" checked>Speed </label>
+                <label><input type="radio" name="option" value="best-route" onclick="setSelectedOption(this)">Best route</label>
+                <label><input type="radio" name="option" value="low-carbon" onclick="setSelectedOption(this)">Low Carbon</label>
+                <label><input type="radio" name="option" value="fewer-transfers" onclick="setSelectedOption(this)">Fewer transfers</label>
+            </div>
+        </div>
+
+        <div>
+            <!-- Button to toggle the dropdown -->
+            <button type="button" id="submit" onclick="handleGoButtonClick()" class="submit-btn">Go</button>
+
+            <!-- Dropdown menu -->
+            <div id="dropdownDiv" class="dropdown-menu stylish-dropdown" style="display: none;">
+                <label for="optionsDropdown"></label><select id="optionsDropdown"></select>
+                <button onclick="selectOption()">Select</button>
+            </div>
+        </div>
+    </form>
+
+    <!-- Map placeholder -->
+    <div class="map-placeholder">
+        <img src="map_placeholder_image.jpeg" alt="Map placeholder" width="100%" height="100%">
     </div>
-</body>
+</div>
+
 <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
 <script src="script.js"></script>
-
+</body>
 </html>
+
 <%--        // fetch('YourServletEndpoint', {--%>
 <%--        //     method: 'GET',  // You can use GET or any other HTTP method--%>
 <%--        //     headers: {--%>
--- webapp/src/main/webapp/script.js
+++ webapp/src/main/webapp/script.js
@@ -1,10 +1,34 @@
-//radiobutton value:
-var selectedOption; // Global variable to store the selected option value
+// Declare routes as a global variable
+var routes;
+// Global variable to store the selected option value
+var selectedOption;
+// Function to handle radio button selection
+//Below is to capture dropdown value and display it in place of map image.
+
 function setSelectedOption(radio) {
-    selectedOption =  radio.value;
-    console.log("radio value selected is "+ selectedOption); //this is getting printed in console of chrome;
+    selectedOption = radio.value;
+    console.log("Selected Option: " + selectedOption);
 }
-// Function to handle form submission
+
+//Function to handle form submission
+function handleGoButtonClick(){
+   // selectOption();
+    toggleDropdown();
+    submitForm();
+   //populateDropdown(routes); // Populate the dropdown with data from the JSON response
+}
+document.getElementById('submit').addEventListener('click', handleGoButtonClick);
+// Listening here for a change in the dropdown selection
+document.getElementById('optionsDropdown').addEventListener('change', function () {
+    const selectedIndex = this.value.split('-');
+    const routeIndex = parseInt(selectedIndex[0]);
+    const legIndex = parseInt(selectedIndex[1]);
+
+    const selectedRoute = routes[routeIndex].legs[legIndex];
+    console.log('Selected Route:', selectedRoute);
+    // Further actions based on the selected route
+    displayRouteDetails(selectedRoute);
+});
 function submitForm() {
     // Retrieve values from the form fields
     var origin = document.getElementById("origin").value;
@@ -43,47 +67,125 @@ function submitForm() {
         .catch(error => {
             console.error('Error:', error);
         });
+
+    // After performing initial actions, call populateDropdown with the parameter 'hi'
+   // populateDropdown('hi');
+
 }
+
 // Fetch data from the server
 document.addEventListener('DOMContentLoaded', function() {
     fetch('RouteOptionServlet')
         .then(response => response.json())
         .then(data => {
+            // Set routes as the received data
+            routes = data;
             // Handle the response
             console.log(data);//testing display
-            populateDropdown(data);
+            populateDropdown(routes);
+
         })
         .catch(error => {
             console.error('Error:', error);
         });
-
-// Function to populate the dropdown with routes
     function populateDropdown(routes) {
-        var dropdown = document.getElementById('routesDropdown');
 
-        // Clear existing options
+        var dropdown = document.getElementById('optionsDropdown');
+
+        // Clear any existing options
         dropdown.innerHTML = '';
 
         // Add each route as an option
-        routes.forEach(function (route) {
-            console.log(route);// test display
-            var option = document.createElement('option');
-            option.value = JSON.stringify(route); // Store the entire route JSON as the value
-            option.text = route.description; // have to adjust this based on  route object structure
+        routes.forEach(function (route, index) {
+            route.legs.forEach(function (leg, legIndex) {
+                const option = document.createElement('option');
+                option.value = index + '-' + legIndex; // Store the entire route and leg index as the value
+                option.text = 'Option ' + (index + 1) + '-' + (legIndex + 1) +
+                    ' (Distance: ' + leg.distance + ', Duration: ' + leg.duration +
+                    ', Cost: ' + leg.cost + ', Mode: ' + leg.mode + ')';
 
-            dropdown.appendChild(option);
+                dropdown.appendChild(option);
+            });
         });
+
     }
 
+    // Listening here for a change in the dropdown selection
+    document.getElementById('optionsDropdown').addEventListener('change', function () {
+        const selectedIndex = this.value;
 
-// Listening here for a change in the dropdown selection
-    document.getElementById('routesDropdown').addEventListener('change', function () {
-        var selectedRoute = JSON.parse(this.value); // Parse the JSON string back to an object
+        const selectedRoute = routes[selectedIndex]; // Assuming 'routes' is a global variable
         console.log('Selected Route:', selectedRoute);
-        // further actions based on the selected route
+        // Further actions based on the selected route
+        displayRouteDetails(selectedRoute);
     });
 
+    // Function to display the details of the selected route
+    function displayRouteDetails(routes) {
+        // Assuming there's an element with id 'routeDetails' to display the details
+        var routeDetailsElement = document.getElementById('routeDetails');
+        routeDetailsElement.innerHTML = JSON.stringify(route, null, 2); // Beautify the JSON string
+    }
 });
+// Function to show/hide the dropdown menu
+function toggleDropdown() {
+    // Check if the mandatory fields (origin and destination) are filled
+    var origin = document.getElementById("origin").value;
+    var destination = document.getElementById("destination").value;
+
+    if (origin.trim() === "" || destination.trim() === "") {
+        // If not filled, alert the user or provide any feedback
+        alert("Please fill in the mandatory fields: Origin and Destination.");
+    } else {
+        // If filled, proceed to toggle the dropdown visibility
+        var dropdown = document.getElementById("optionsDropdown");
+
+        if (dropdown.style.display === "block") {
+            dropdown.style.display = "none"; // Hide the dropdown when it's already open
+        } else {
+            dropdown.style.display = "block"; // Show the dropdown when the "Go" button is clicked
+        }
+    }
+}
+//replaces populating Dropdown; image with the selected option information from the drowpdown
+// Replaces populating Dropdown; image with the selected option information from the dropdown
+
+    function selectOption() {
+        var dropdown = document.getElementById("optionsDropdown");
+        var selectedOptionIndex = dropdown.options[dropdown.selectedIndex].value;
+
+        console.log("Selected Option: " + selectedOptionIndex);
+
+        // Remove the previous option text and image if they exist
+        const previousOptionText = document.querySelector('.map-placeholder .option-text');
+        if (previousOptionText) {
+            previousOptionText.remove();
+        }
+
+        // Check if the selected route has 'legs' property
+        if (routes[selectedOptionIndex].legs && routes[selectedOptionIndex].legs.length > 0) {
+            var legs = routes[selectedOptionIndex].legs[0];
+
+            // Create a new element for the selected option text
+            var optionText = document.createElement('div');
+            optionText.textContent = "Time: " + (legs.duration || 0) + " mins, Distance: " + (legs.distance || 0) + " miles, Transits: " + (legs.transits || 0);
+            optionText.className = "option-text";
+            optionText.style.fontSize = "16px";
+            optionText.style.textAlign = "center";
+            optionText.style.marginTop = "20px";
+
+            // Add the new option text to the map placeholder
+            var mapPlaceholder = document.querySelector('.map-placeholder');
+            mapPlaceholder.appendChild(optionText);
+        } else {
+            console.error("Selected route does not have the expected structure.");
+        }
+
+        // Hide the dropdown after selection
+        var dropdownHide = document.getElementById("dropdownDiv");
+        dropdownHide.style.display = "none";
+
+}
 // angular.module('routeApp', [])
 //     .controller('RouteController', function ($scope, $http) {
 //         // Simulate receiving routes from the backend
